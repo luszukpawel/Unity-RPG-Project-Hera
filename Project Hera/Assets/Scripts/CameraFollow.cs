f4d67cfcf6084c1582ca058acc1477bc06cb0b33
pluszak <luszukpawel@gmail.com>
2017-06-06 00:37:35 +0200

    Created Project + Standard Assets

---
+++ Project Hera/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private GameObject PlayerTransform;
+    void Start()
+    {
+        PlayerTransform = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void LateUpdate()
+    {
+        transform.position = PlayerTransform.transform.position;
+    }
+}
